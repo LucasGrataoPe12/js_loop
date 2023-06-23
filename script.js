86a7b41245a80ef651603d33f0071cadd2d4803a
LucasGrataoPe12 <108188844+LucasGrataoPe12@users.noreply.github.com>
2023-06-23 16:37:43 -0300

    tomi

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ while (contador <= 10) {
 }
 //for
 console.log('for');
-for (let contador2 = 0; contador2 <=10; contador2++) {
+for (let contador2 = 0; contador2 <= 10; contador2++) {
     let resultado = tabuada * contador2;
     console.log(tabuada + ' X ' + contador2 + ' = ' + resultado);
 }
+//do while
+console.log('do while');
+
+let contador3 =  0;
+do {
+    let resultado  = tabuada * contador3;
+    console.log(tabuada + ' X ' + contador3 + ' X ' + resultado);
+    contador3++;
+} while (contador3 <= 10);
